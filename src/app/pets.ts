639c99853b2f2bd5b7c908a812967d187b9f7af8
21e8 <alize_in_cryptoland@protonmail.com>
2024-05-14 10:52:39 +0200

    init

---
+++ src/app/pets.ts
@@ -0,0 +1,92 @@
+export const PETS = [
+  {
+    mint: "5oVNBeEEQvYi1cX3ir8Dx5n1P7pdxydbGF2X4TxVusJm",
+    image: "/INF_v1.webp",
+    name: "Infinitie",
+  },
+  {
+    mint: "BonK1YhkXEGLZzwtcvRTip3gAL9nCeQD7ppZBLXhtTs",
+    image: "/bonkSOL_v1.webp",
+    name: "Bonksie",
+  },
+  {
+    mint: "Comp4ssDzXcLeu2MnLuGNNFC4cmLPMng8qWHPvzAMU1h",
+    image: "/compassSOL_v1.webp",
+    name: "Compie",
+  },
+  {
+    mint: "Dso1bDeDjCQxTrWHqUUi63oBvV7Mdm6WaobLbQ7gnPQ",
+    image: "/dSOL_v1.webp",
+    name: "Driftie",
+  },
+  {
+    mint: "GRJQtWwdJmp5LLpy8JWjPgn5FnLyqSJGNhn5ZnCTFUwM",
+    image: "/clockSOL_v1.webp",
+    name: "Clockie",
+  },
+  {
+    mint: "HUBsveNpjo5pWqNkH57QzxjQASdTVXcSK7bVKTSZtcSX",
+    image: "/hubSOL_v1.webp",
+    name: "Hubbie",
+  },
+  {
+    mint: "LAinEtNLgpmCP9Rvsf5Hn8W6EhNiKLZQti1xfWMLy6X",
+    image: "/laineSOL_v1.webp",
+    name: "Lainie",
+  },
+  {
+    mint: "LnTRntk2kTfWEY6cVB8K9649pgJbt6dJLS1Ns1GZCWg",
+    image: "/lanternSOL_v1.webp",
+    name: "Lanternie",
+  },
+  {
+    mint: "he1iusmfkpAdwvxLNGV8Y1iSbj4rUy6yMhEA3fotn9A",
+    image: "/hSOL_v1.webp",
+    name: "Helie",
+  },
+  {
+    mint: "jucy5XJ76pHVvtPZb5TKRcGQExkwit2P5s4vY8UzmpC",
+    image: "/jucySOL_v1.webp",
+    name: "Jucie",
+  },
+  {
+    mint: "jupSoLaHXQiZZTSfEWMTRRgpnyFm8f6sZdosWBjx93v",
+    image: "/jupSOL_v1.webp",
+    name: "Jupitie",
+  },
+  {
+    mint: "pWrSoLAhue6jUxUkbWgmEy5rD9VJzkFmvfTDV5KgNuu",
+    image: "/pwrSOL_v1.webp",
+    name: "Powie",
+  },
+  {
+    mint: "pathdXw4He1Xk3eX84pDdDZnGKEme3GivBamGCVPZ5a",
+    image: "/pathSOL_v1.webp",
+    name: "Teafsie",
+  },
+  {
+    mint: "picobAEvs6w7QEknPce34wAE4gknZA9v5tTonnmHYdX",
+    image: "/picoSOL_v1.webp",
+    name: "Picolo",
+  },
+  {
+    mint: "st8QujHLPsX3d6HG9uQg9kJ91jFxUgruwsb1hyYXSNd",
+    image: "/stakeSOL_v1.webp",
+    name: "Stakie",
+  },
+  {
+    mint: "strng7mqqc1MBJJV6vMzYbEqnwVGvKKGKedeCvtktWA",
+    image: "/strongSOL_v1.webp",
+    name: "Stonkie",
+  },
+  {
+    mint: "suPer8CPwxoJPQ7zksGMwFvjBQhjAHwUMmPV4FVatBw",
+    image: "/superSOL_v1.webp",
+    name: "Superfastie",
+  },
+  {
+    mint: "vSoLxydx6akxyMD9XEcPvGYNGq6Nn66oqVb3UkGkei7",
+    image: "/vSOL_v1.webp",
+    name: "Vaultie",
+  },
+];
