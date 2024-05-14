289bd40f0ee50b8e18aa1ae62a20042e72acb89e
21e8 <alize_in_cryptoland@protonmail.com>
2024-05-14 12:50:59 +0200

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { GithubIcon } from "./github-icon";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -60,6 +61,7 @@ export default function RootLayout({
             </i>
           </a>
         </div>
+        <Analytics />
       </body>
     </html>
   );
