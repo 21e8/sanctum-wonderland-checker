e4b328f252a3bbf3a84cc0bc11f3c208ede9eb4f
21e8 <alize_in_cryptoland@protonmail.com>
2024-05-14 11:58:48 +0200

    loading screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
 import { Suspense } from "react";
 import App from "./_app";
+import { Logo } from "./logo";
 
+const Loading = () => {
+  return (
+    <div className="w-screen h-screen flex flex-col items-center justify-center gap-6">
+      <div className="flex flex-col gap-4 items-center justify-center">
+        <Logo />
+        <h1 className="text-4xl font-bold text-center">Wonderland Checker</h1>
+      </div>
+      <h2 className="text-lg">Loading...</h2>
+    </div>
+  );
+};
 export default function Home() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<Loading />}>
       <App />
     </Suspense>
   );
