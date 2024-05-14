8c41d4a5bf06b94e69ba03e32b73ab2d87215135
21e8 <alize_in_cryptoland@protonmail.com>
2024-05-14 12:59:07 +0200

    leave out createdAt

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -30,8 +30,6 @@ export default function App() {
   const router = useRouter();
   const searchParams = useSearchParams();
   const [loading, setLoading] = useState(false);
-  const [displayCreatedAt, setDisplayCreatedAt] = useState(false);
-
   const getInfo = useCallback(
     async (address: string) => {
       router.push(`/?address=${address}`);
@@ -49,11 +47,6 @@ export default function App() {
         }
         const json: SanctumResponse = await res.json();
         setInfo(json);
-        if (json.createdAt === 0) {
-          setDisplayCreatedAt(false);
-        } else {
-          setDisplayCreatedAt(true);
-        }
         setLoading(false);
       } catch (e: any) {
         setError(e.message);
@@ -108,14 +101,6 @@ export default function App() {
         <>
           <h2 className="text-2xl font-bold text-center">Stats</h2>
           <div className="grid grid-cols-2 gap-4 border border-black dark:border-white px-3 py-2 rounded-lg">
-            {displayCreatedAt ? (
-              <>
-                <div>Created at:</div>
-                <div>{new Date(info.createdAt).toLocaleString()}</div>
-              </>
-            ) : (
-              <></>
-            )}
             <div>Last Recorded Epoch:</div>
             <div>{info.lastRecordedEpoch}</div>
             <div>Global Rank:</div>
